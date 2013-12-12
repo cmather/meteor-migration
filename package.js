78a2b248799ec1eac77d2e08c9652e9489fe3fef
Chris Mather <mather.chris@gmail.com>
2013-12-12 12:37:56 -0800

    Add underscore dependency

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+  api.use('underscore');
   api.use('reload');
   api.add_files('migration.js', 'client');
   api.export('onMigrate', 'client');
